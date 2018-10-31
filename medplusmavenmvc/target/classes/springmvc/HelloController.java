1f127e0726078a0c944d5a46a80c8324768e09b1
Manoj.y <manoj.y@kustommade.com>
2018-10-31 17:08:46 +0530

    adding maven application to git

---
+++ medplusmavenmvc/target/classes/springmvc/HelloController.java
@@ -0,0 +1,13 @@
+package springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String welcome() {
+		return "Welcome";
+	}
+}
